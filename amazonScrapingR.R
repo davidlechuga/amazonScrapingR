a23844244fdb239e8750bed697d281a03a11ccfb
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-12-27 16:18:25 -0600

    funcion para extraer los datos de las subURLS automaticamente

--- amazonScrapingR.R
+++ amazonScrapingR.R
@@ -1,7 +1,13 @@
-############## EXTRAER LA URL DEL PRIMERO OBJETO EN LISTA ###########################
+####################################################################################
+##########################                          ################################
+#############################                   ####################################
+################################# PROCESO I #######################################
+
+############## EXTRAER LA URL DEL PRIMER OBJETO EN LISTA ###########################
 
 library(rvest)
-url<- "https://www.amazon.es/s?k=aspiradora&dc&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1577397985&ref=sr_ex_p_89_0"
+
+url<-"https://www.amazon.es/s?k=aspiradora&dc&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1577397985&ref=sr_ex_p_89_0"
 selector<-"#search > div.s-desktop-width-max.s-desktop-content.sg-row > div.sg-col-20-of-24.sg-col-28-of-32.sg-col-16-of-20.sg-col.s-right-column.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(5) > div.s-result-list.s-search-results.sg-row > div:nth-child(1) > div > span > div > div > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-16-of-24.sg-col-12-of-20.sg-col-24-of-32.sg-col.sg-col-28-of-36.sg-col-20-of-28 > div > div:nth-child(1) > div > div > div:nth-child(1) > h2 > a"
 #leer la pagina html
 pagina<-read_html(url)
@@ -119,3 +125,101 @@ str(res_tabla)
 resultado_aspiradoras <- c(nombre_texto,precio_texto,opiniones_texto,as.character(res_tabla$`Peso del producto`), as.character(res_tabla$Potencia), as.character(res_tabla$`Dimensiones del producto`), as.character(res_tabla$Marca))
 resultado_aspiradoras
 class(resultado_aspiradoras)
+####################################################################################
+##########################                          ################################
+#############################                   ####################################
+################################# PROCESO II #######################################
+
+getArticulo <- function (url){
+  
+  ######################### EXTRAER titulo del Producto ############################### 
+  
+  nombre <- "#productTitle"
+  pagina_web <- read_html(url)
+  nombre_nodo<- html_node(pagina_web, nombre)
+  nombre_texto <- html_text(nombre_nodo)
+  nombre_texto
+  ######################### EXTRAER Opiniones del Producto ############################### 
+  
+  opiniones <- "#acrCustomerReviewText"
+  opiniones_nodo<- html_node(pagina_web, opiniones)
+  opiniones_texto <- html_text(opiniones_nodo)
+  opiniones_texto
+  
+  
+  ######################### EXTRAER Precio del Producto ############################### 
+  
+  precio <- "#priceblock_ourprice"
+  precio_nodo<- html_node(pagina_web, precio)
+  precio_texto <- html_text(precio_nodo)
+  precio_texto
+  
+  ######################### EXTRAER Tabla de Descripcion del Producto ############################### 
+  
+  tabla <- "#prodDetails > div > div.column.col1 > div > div.content.pdClearfix > div > div > table"
+  tabla_nodo<- html_node(pagina_web, tabla)
+  tabla_tab <- html_table(tabla_nodo)
+  tabla_tab
+  class(tabla_tab)
+  ### Modificar el dataFrame var1 col var2 row en un vector
+  val <- tabla_tab$X2
+  val
+  ##tranformada de val 
+  res_tabla <- data.frame(t(val))
+  res_tabla
+  tabla_name <- tabla_tab$X1
+  tabla_name
+  #colocar los nombres de columnas 
+  colnames(res_tabla) <- tabla_name
+  res_tabla
+  class(res_tabla)
+  str(res_tabla)
+  col <- c("Peso del producto ", "Dimensiones del producto", "Marca", "Potencia")
+  
+  if(length(res_tabla) == 0 ) {
+    #NO hay detalles todo a menos -1
+    mitab<- data.frame(colnames(col))
+    mitab<-rbind(mitab, c("-1","-1","-1","-1"))
+    colnames(mitab)<- col
+    col
+    mitab
+  } else {
+    #Evaluar cada uno de los atributos (INTERESA O NO NOS INTERESA).
+    zero<- matrix("-1", ncol = 4, nrow = 1)
+    dfzero<- as.data.frame(zero)
+    # poniendole los nombres de col al DATAFRAME
+    colnames(dfzero)<- col
+    dfzero
+    #el Data Frame lo convertimos en character para amnipular la matriz
+    
+    peso<- as.character(res_tabla$`Peso del producto`)
+    #condicionales para PESO
+    if (length(peso)== 0) peso <- "-1"
+    
+    dimen<- as.character(res_tabla$`Dimensiones del producto`)
+    #condicionales para DIMENSIONES
+    if (length(dimen)== 0) dimen <- "-1"
+    
+    marca<- as.character(res_tabla$`Marca`)
+    #condicionales MARCA
+    if (length(marca)== 0) marca <- "-1"
+    
+    potencia<- as.character(res_tabla$`Potencia`)
+    #condicionales para POTENCIA
+    if (length(potencia)== 0) potencia <- "-1"
+    
+    ##Asignamos los valores de nuestras variables a nuestro dataFrame
+    dfzero
+    dfzero$`Peso del producto` <- peso
+    dfzero$`Dimensiones del producto` <- dimen
+    dfzero$`Marca` <- marca
+    dfzero$`Potencia`<- potencia
+    mitab <- dfzero
+    colnames(mitab) <- col
+  }
+  articulo <- c(nombre_texto,precio_texto,opiniones_texto, as.character(mitab$`Peso del producto`[1]), as.character(mitab$`Dimensiones del producto`), as.character(mitab$Marca), as.character(mitab$Potencia))
+  articulo
+}
+
+res<- getArticulo(vlinkAspiradora[2])
+res
